de2183109269fcec8fcd22257af23c5d98a8702e
aepyornis <ziggy@elephant-bird.net>
2014-12-24 00:19:40 -0500

    basic map template

---
+++ map.js
@@ -0,0 +1,55 @@
+//create map
+var map = L.map('map', {
+}).setView([40.669126, -73.918670], 12);
+
+//basemaps
+
+var osmMap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'open street map'
+        });
+
+var toner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>,<a href="http://creativecommons.org/licenses/by/3.0"> CC BY 3.0</a> &mdash; Map data: OSM',
+        }).addTo(map);
+
+var styles = {  
+  blank: {
+        fillOpacity: 0,
+        opacity: 0,
+        fillColor: '#ff0000' 
+        },
+  polygon: {
+        fillColor: '#ff0000',
+        fillOpacity: 0.9
+    }
+
+}
+
+//layer control
+//adds Control to map-> must happen in Ajax callback
+function addControl() {
+    var baseMaps = {
+        "Open Street Map": osmMap,
+        "Black and White": toner
+    };
+    var overLays = {
+      
+    };
+
+    L.control.layers(baseMaps, overLays, {position: "topright"}).addTo(map);
+
+}
+
+//running
+$( document).ready(function(){
+  addControl();
+  addCostSlider();
+});
+
+//slider
+
+function addCostSlider() {
+  $("#cost_slider").slider({
+  });
+  console.log("cost slider added?");
+}
