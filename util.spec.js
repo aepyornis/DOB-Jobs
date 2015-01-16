3f1d5a5a881e3366a3b2b6c353e82b870b2b8614
aepyornis <ziggy@elephant-bird.net>
2015-01-15 19:10:04 -0500

    util2.js uses forEacah instead of cursor

--- util.spec.js
+++ util.spec.js
@@ -1,20 +1,18 @@
 var fs = require('fs');
 var should = require('should');
-var utils = require('./util');
+var utils = require('./util2');
 // var db = mongojs('mongodb://localhost:27017/test', ['jobs']);
 
 describe('writeCSV', function(){
-  this.timeout(0);
+  this.timeout(2);
 
     before(function(done){
-       this.timeout(0);
       utils.writeCSV(['BuildingType', 'ExistingStories', 'OwnerName'], 'testme', {'CB': '304'}, function(){
               done()
         })
     })
 
     it('works on three variables', function(){
-       this.timeout(0);
             var test_csv = fs.readFileSync('testme.csv').toString();
             var lines = test_csv.split(/\n/);
             lines[0].should.eql('BuildingType,ExistingStories,OwnerName');
