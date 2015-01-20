c4544a3d5a1302534936639fff92822031bcdf44
Ziggy <ziggy@elephant-bird.net>
2015-01-19 19:42:41 -0500

    connection to postgres works

--- postgres_parse/parse.js
+++ postgres_parse/parse.js
@@ -1,6 +1,34 @@
 //node parse from excel to to postgres
 var fs = require('fs');
 var XLS = require('xlsjs');
+var pg = require('pg');
+
+
+//connect to postgres
+var client = new pg.Client('postgres://mrbuttons@localhost/dob');
+
+do_some_SQL('SELECT NOW() As "theTime"', function(result){
+    console.log(result.rows[0]);
+});
+
+//this function excutes sql
+function do_some_SQL (sql, callback) {
+    client.connect(function(err){
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query(sql, function(err, result){
+            if (err) {
+                return console.error('query error', err)
+            }
+            //this disconnects from the database
+            client.end();
+            callback(result);
+        })
+    })
+}
+
+
 
 function create_excel_files_arr(filePath) {
         var allFiles = fs.readdirSync(filePath);
@@ -12,6 +40,15 @@ function create_excel_files_arr(filePath) {
         })
         return onlyExcel;
 }
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     create_excel_files_arr: create_excel_files_arr
