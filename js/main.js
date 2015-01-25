0fcba6b3c7663c0e7756eee70632bc56b12b05df
Ziggy <ziggy@elephant-bird.net>
2015-01-25 13:19:58 -0500

    switch to datatables

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
 $( document ).ready(function() {
+
   $.ajax({
     url: 'http://localhost:3000/query',
     // dataType: 'json',
     type: 'GET',
   })
   .then(function(data){
-    createBootstrapTable(JSON.parse(data));
+    createTable(JSON.parse(data));
   })
   .fail(function(jqXHR, status){
     console.log(status)
     });
 })
 
-function createBootstrapTable(tableData) {
-  $('#table').bootstrapTable({
-    data: tableData
+function createTable(tableData) {
+
+  $('#table').DataTable( {
+    data: tableData,
+    columns: [
+        { data: 'house' },
+        { data: 'streetname' },
+        { data: 'bbl' },
+        { data: 'latestactiondate' },
+        { data: 'buildingtype' },
+        { data: 'existstories' },
+        { data: 'proposedstories' },
+        { data: 'ownername' },
+        { data: 'ownerbusinessname' },
+        { data: 'jobdescription' }
+    ]
   });
+
 }
+
+
+// function setDefaults(){
+
+//   $.fn.bootstrapTable.columnDefaults.sortable = true;
+//   // jQuery.fn.bootstrapTable.columnDefaults.
+
+// }
+
+// function createBootstrapTable(tableData) {
+//   $('#table').bootstrapTable({
+//     data: tableData,
+//     pageSize: 5,
+//     pagination: true
+//   });
+// }
