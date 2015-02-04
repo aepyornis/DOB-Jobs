51819bfc1f95267230a282f123e505296f5c4ba6
Chris Henrick <chrishenrick@gmail.com>
2015-02-04 17:53:05 -0500

    remove old mongo version

--- mongo_version/util/write_csv_test.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var utils = require('./util');
-
-utils.writeCSV(['BuildingType', 'ExistingStories', 'OwnerName'], 'writecsv_test', {'CB': '304'}, function(){
-              console.log('done')
-        })
