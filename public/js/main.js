689c1a220bfb4692453639ea0a6218faa43e6081
aepyornis <ziggy@elephant-bird.net>
2015-02-05 09:52:29 -0500

    more columns

--- public/js/main.js
+++ public/js/main.js
@@ -6,43 +6,65 @@ $( document ).ready(function() {
       url: '/datatables',
       type: 'POST'
     },
-     "order": [[ 3, "desc" ]],
+     "order": [[ 2, "desc" ]],
     columns: [
-        { data: 'house',
-          "searchable": false,
-          "orderable": false 
-        },
-        { data: 'streetname',
-        "orderable": false 
-        },
-        { data: 'bbl', 
-          "searchable": false,
-          "orderable": false
-        },
-        { data: 'latestactiondate',
-          "searchable": false
-        },
-        { data: 'jobtype' 
-        },
-        { data: 'existstories', 
-          "searchable": false
-        },
-        { data: 'proposedstories',
-          "searchable": false
-         },
-        { data: 'ownername' },
-        { data: 'ownerbusinessname' },
-        { data: 'jobdescription' }
+       {
+        data: 'house',
+        'searchable': false,
+        'orderable': false
+       },
+       {
+        data: 'streetname',
+        orderable: false
+       },
+       {
+        data: 'latestactiondate',
+        searchable: false
+       },
+       {
+        data: 'cb',
+        searchable: false,
+        orderable: false
+       },
+       {
+        data: 'jobtype',
+        searchable: false
+       },
+       {
+        data: 'jobdescription',
+        orderable: false
+       },
+       {
+        data: 'ownername'
+       },
+       {
+        data: 'ownerbusinessname'
+       },
+       {
+        data: 'existstories',
+        searchable: false
+       },
+       {
+        data: 'proposedstories',
+        searchable: false
+       },
+       {
+        data: 'existdwellunits',
+        searchable: false
+       },
+       {
+        data: 'proposeddwellunits',
+        searchable: false
+       },
+       {
+        data: 'initialcost',
+        searchable: false
+       }
     ]
   });
 
-  yadcf.init(table, [
-    {column_number : 1, filter_type: "text"},
-    {column_number : 3, filter_type: "date"},
-    {column_number : 4, column_data_type: "multi_select", data: ['A1', 'A2', 'A3', 'NB']},
-    {column_number : 5, filter_type: "range_number", filter_delay: 300},
-    {column_number : 6, filter_type: "range_number", filter_delay: 300},
-    {column_number : 7, filter_type: "text", filter_delay: 300}
-  ]);
+  // yadcf.init(table, [
+    
+  // ]);
 
 })
